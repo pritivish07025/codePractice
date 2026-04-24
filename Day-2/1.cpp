c5d8d5fdb92564f647714d430028d7f0109c9846
Priti Vishwakarma <priti.vishwakarma311664@gmail.com>
2026-04-24 12:23:19 +0530

    commit

---
+++ Day-2/1.cpp
@@ -0,0 +1,15 @@
+// Assignment Operator 
+#include <iostream>
+using namespace std ;
+int main(){ 
+int ans=(5/(double)2);
+cout<<ans<<endl;
+cout<<(5/(double)2)<<endl;
+cout<< 3+5<<endl;
+cout<< 3-5<<endl;
+cout<< 3*5<<endl;
+cout<< 3/5<<endl;
+cout<< 3%5<<endl;
+
+
+}
