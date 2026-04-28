15c4b8f1b2d5f1048990f4a58267eaed72bfe97a
Priti Vishwakarma <priti.vishwakarma311664@gmail.com>
2026-04-28 20:02:22 +0530

    commit

---
+++ Day-3/3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // program to find out whether a number is positive or negative
+    int n;
+    cout<<"enter number:"<<endl;
+    cin>>n;
+    if(n>0){
+        cout<<"positive number"<<endl;
+
+    }
+    else{
+        cout<<"negative number";
+    }
+}
