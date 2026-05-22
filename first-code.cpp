9ad1599c88700759586c20067fe50db5994592c0
Priti Vishwakarma <priti.vishwakarma311664@gmail.com>
2026-05-22 17:55:55 +0530

    commit

---
+++ first-code.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Priti Vishwakarma";
+    return 0;
+}
---
+++ linear-search.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+
+    // Input size of array
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    // Input array elements
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Element to search
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    // Linear Search
+    bool found = false;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            cout << "Element found at index " << i << endl;
+            found = true;
+            break;
+        }
+    }
+
+    // If element not found
+    if(!found) {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
