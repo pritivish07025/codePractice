bb2ef594f8ad56f9a84a4ce964023e5270cba0ad
Priti Vishwakarma <priti.vishwakarma311664@gmail.com>
2026-07-19 14:02:21 +0530

    commit

---
+++ contain-duplicate.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool containsDuplicate(vector<int>& nums) {
+    int n = nums.size();
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(nums[i] == nums[j]) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 1};
+
+    if(containsDuplicate(nums))
+        cout << "True";
+    else
+        cout << "False";
+
+    return 0;
+}
