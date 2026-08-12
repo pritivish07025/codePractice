95c35a44b18d824067059e6daeddbae52cd4509e
Priti Vishwakarma <priti.vishwakarma311664@gmail.com>
2026-08-12 21:46:56 +0530

    commit

--- addition-of-two-matrix.cpp
+++ addition-of-two-matrix.cpp
@@ -18,7 +18,7 @@ int main() {
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             cin >> A[i][j];
-        }
+        } 
     }
 
     // Input second matrix
@@ -46,4 +46,4 @@ int main() {
     }
 
     return 0;
-}
+}  
