31b9fa7f0cc4c7d37932417fa8cc6e32e42f59c1
Priti Vishwakarma <priti.vishwakarma311664@gmail.com>
2026-09-02 21:53:30 +0530

    Create 13q.cpp

---
+++ 13q.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return 1;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums1 = {1, 2, 3, 4};
+
+    bool result = sol.uniformArray(nums1);
+
+    cout << boolalpha << result << endl;
+
+    return 0;
+}
